b13d0e8ab0fb961dc04f73f1d3062250789af41c
USER 2020 <hackingetico2021@gmail.com>
2022-04-15 21:49:25 -0500

    15-04-2022

--- EntidadFinanciera/src/vista/PrincipalClientes.java
+++ EntidadFinanciera/src/vista/PrincipalClientes.java
@@ -25,31 +25,42 @@ public class PrincipalClientes extends Usuario{
     public void leerDatosCliente()
     {
         //Ejercicio deben insertar datos en el arreglo.
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el nombre del usuario: ");
-        nombre = sc.nextLine();
-        System.out.println("Ingrese el apellido del usuario: ");
-        apellido = sc.nextLine();
-        System.out.println("Ingrese la direccion del usuario: ");
-        direccion = sc.nextLine();
-        System.out.println("Ingrese el telefono del usuario: ");
-        telefono = sc.nextInt();
-        System.out.println("Ingrese el correo del usuario: ");
-        correo = sc.nextLine();
-        System.out.println("Ingrese la cedula del usuario: ");
-        cedula = sc.nextInt();
-        ArrayList<String> lista1 = new ArrayList<String>();
-        lista1.add(nombre);
-        lista1.add(apellido);
-        lista1.add(direccion);
-        lista1.add(telefono, nombre);
-        lista1.add(correo);
-        lista1.add(cedula, nombre);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Ingrese el nombre del usuario: ");
+//        nombre = sc.nextLine();
+//        System.out.println("Ingrese el apellido del usuario: ");
+//        apellido = sc.nextLine();
+//        System.out.println("Ingrese la direccion del usuario: ");
+//        direccion = sc.nextLine();
+//        System.out.println("Ingrese el telefono del usuario: ");
+//        telefono = sc.nextInt();
+//        System.out.println("Ingrese el correo del usuario: ");
+//        correo = sc.nextLine();
+//        System.out.println("Ingrese la cedula del usuario: ");
+//        cedula = sc.nextInt();
+//        ArrayList<String> lista1 = new ArrayList<String>();
+//        lista1.add(nombre);
+//        lista1.add(apellido);
+//        lista1.add(direccion);
+//        lista1.add(telefono, nombre);
+//        lista1.add(correo);
+//        lista1.add(cedula, nombre);
+//          this.nombre = nombre;
+//          this.apellido = apellido;
+//          this.cedula = cedula;
+//          this.correo = correo;
+//          this.telefono = telefono;
+          //contrcli.insertar(this.nombre, this.apellido, this.cedula, this.correo, this.direccion, this.telefono);//porque no sirve llenar el objeto asi?
+          contrcli.insertar(nombre = "Juan");
+          contrcli.insertar(apellido = "Zuluaga");
+          contrcli.insertar(cedula = 1053863921);
+          contrcli.insertar(direccion = "Cra 14");
+          contrcli.insertar(telefono = 305714941);
     }
     
     public void menu()
     {
-        int opc=0;
+        int opc=1;
         System.out.println("Menu Insertar datos de Clientes");
         System.out.println("1. Insertar");
         System.out.println("2. Consultar");
@@ -57,9 +68,12 @@ public class PrincipalClientes extends Usuario{
         System.out.println("4. Borrar");
         switch(opc)
         {
-            case   0:
+            case   1:
                 leerDatosCliente();
             default :
+                
+            case 2:
+                
         }
                 
     }
